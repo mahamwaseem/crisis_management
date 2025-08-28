89ffff1db95e3a8a63494ae7db34c340ad957636
Maham Waseem <mahamw440@gmail.com>
2025-08-28 19:46:05 +0500

    Updated path of files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from "./myComponents/header";
-import HeroSection from "./myComponents/heroSection";
-import AboutSection from "./myComponents/AboutSection";
-import Footer from './myComponents/footer';
+import Header from "./pages//header";
+import HeroSection from "./pages/heroSection";
+import AboutSection from "./pages/AboutSection";
+import Footer from './pages/footer';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 

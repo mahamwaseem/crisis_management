9b03c6be4299d77a5fd34e27f99035eb9c58e014
Maham Waseem <mahamw440@gmail.com>
2025-09-10 19:53:28 +0500

    Added path /:reportId to  GetComment

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -10,7 +10,7 @@ const commentSchema = new mongoose.Schema(
 
     userId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "users",
+      ref: "User",
       required: true
     },
 
--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,8 +1,9 @@
 const authMiddleware = require("../Middlewares/authMiddleware");
-const { addComment } = require("../Controllers/commentController")
+const { addComment, getComments } = require("../Controllers/commentController")
 
 const router = require("express").Router();
 
 router.post('/', authMiddleware, addComment);
+router.get('/:reportId', authMiddleware, getComments);
 
 module.exports = router;

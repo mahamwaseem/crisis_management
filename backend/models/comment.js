562ef0b28d09d0e1c07d6125921e698963b444d2
Maham Waseem <mahamw440@gmail.com>
2025-09-10 18:42:47 +0500

    Add Comment schema
    - Created Comment model with fields:
      - reportId (ref → Report)
      - userId (ref → User)
      - text (comment body)
    - Added timestamps for createdAt and updatedAt

---
+++ backend/models/comment.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+  {
+    reportId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Reports",
+      required: true
+    },
+
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true
+    },
+
+    text: {
+      type: String,
+      required: true,
+      trim: true
+
+    },
+  },
+  {timestamps: true}
+);
+module.exports= mongoose.model("Comment", commentSchema);

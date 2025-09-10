dedb696ef37eb36e70bbe46a4d535345b3eff7a7
Maham Waseem <mahamw440@gmail.com>
2025-09-10 18:54:19 +0500

    Create commentController.js

---
+++ backend/Controllers/commentController.js
@@ -0,0 +1,31 @@
+const Comment = require("../models/comment");
+
+const addComment = async(req, res) => {
+  try{
+    const { reportId, text} = req.body;
+
+    if( !reportId || !text){
+      return res.status(400).json({
+        success: false,
+        message: " ReportID and text is required"
+      });
+    }
+
+    const comment = await Comment.create({
+      reportId,
+      userId: req.user.id,
+      text,
+    });
+
+    res.status(200).json({
+      success: true,
+      data: comment
+    });
+  }catch(err){
+    console.error("Error in adding comments", err);
+    res.status(500).json({
+      success: false,
+      message: "Server error while adding comment"
+    });
+  }
+};

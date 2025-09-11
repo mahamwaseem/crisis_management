d7f86d1811a185d85ef7a9a77ff3956895622e59
Maham Waseem <mahamw440@gmail.com>
2025-09-11 18:33:20 +0500

    Added path of delete comment

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,9 +1,10 @@
 const authMiddleware = require("../Middlewares/authMiddleware");
-const { addComment, getComments } = require("../Controllers/commentController")
+const { addComment, getComments, deleteComment } = require("../Controllers/commentController")
 
 const router = require("express").Router();
 
 router.post('/', authMiddleware, addComment);
 router.get('/:reportId', authMiddleware, getComments);
+router.delete("/:id", authMiddleware, deleteComment);
 
 module.exports = router;

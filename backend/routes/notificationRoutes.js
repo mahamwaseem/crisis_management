bedc2d346faceb9eea6ca9bafbf745d0ef52e87c
Maham Waseem <mahamw440@gmail.com>
2025-09-11 19:34:21 +0500

    Add path of delete notification

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { getNotifications, markAsRead} = require("../Controllers/notificationController");
+const { getNotifications, markAsRead, deleteNotification} = require("../Controllers/notificationController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 router.get("/", authMiddleware, getNotifications);
 router.put("/:id/read", authMiddleware, markAsRead);
+router.delete("/:id", authMiddleware, deleteNotification);
 
 
 module.exports = router;
